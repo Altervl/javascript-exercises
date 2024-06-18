a47d6c7223cddccd638e9361a5422d3384c244de
Altervl <62514019+Altervl@users.noreply.github.com>
2024-06-18 22:33:00 +0300

    Complete  exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let str = string;
+    if (times > 0) {
+        for (i = 1; i < times; i++) {
+            str += string;
+        };
+    } else if (times < 0) {
+        str = "ERROR";
+    }  else {
+        str = "";
+    };
+    return str;
 };
 
 // Do not edit below this line
