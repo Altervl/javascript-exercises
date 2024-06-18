02aac9506ed8e7577b669291aa0b0dca966d3a48
Altervl <62514019+Altervl@users.noreply.github.com>
2024-06-18 23:40:07 +0300

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reversed = [];
+    for (char of string.split("")) {
+        reversed.unshift(char);
+    };
+    return reversed.join("");
 };
 
 // Do not edit below this line
