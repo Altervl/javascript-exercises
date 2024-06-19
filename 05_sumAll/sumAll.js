eafd370375a405f02b04f38e1decde620858b9b3
Altervl <62514019+Altervl@users.noreply.github.com>
2024-06-19 18:41:32 +0300

    Done exersices 4 and 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if ((arg1 < 0 || arg2 < 0) 
+     || (typeof arg1 !== "number" || typeof arg2 !== "number")) {
+        return "ERROR";
+    };
 
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+
+    if (arg1 > arg2) {
+        start = arg2;
+        end = arg1;
+    } else {
+        start = arg1;
+        end = arg2;
+    };
+
+    for (i = start; i <= end; i++) {
+        sum += i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
