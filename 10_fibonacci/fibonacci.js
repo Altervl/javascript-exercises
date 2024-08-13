92945c5d30980c685819ce54f918ba447bf53071
Altervl <62514019+Altervl@users.noreply.github.com>
2024-08-13 21:03:21 +0300

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+    const arr = [1, 1];
+        for (i = 2; i < num; i++) {
+            arr.push(arr.at(-1) + arr.at(-2));
+        };
+        return arr.at(-1);
+    };
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
