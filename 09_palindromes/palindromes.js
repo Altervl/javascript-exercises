8286606912e231b282cc9e7ab241a1f62af19a7b
Altervl <62514019+Altervl@users.noreply.github.com>
2024-08-13 14:33:37 +0300

    Complete exercise 09_polindromes
    Also fix an error in the last test: result must be "true".

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const filtered = word.toLowerCase().split("").filter(char => char.toUpperCase() != char.toLowerCase());
+  const reversed = filtered.toReversed();
+  return filtered.join() === reversed.join() ? true : false;
 };
 
 // Do not edit below this line
