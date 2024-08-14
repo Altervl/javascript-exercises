e1a853edcfc624ce17905085e6fe0f5040fd5a2e
Altervl <62514019+Altervl@users.noreply.github.com>
2024-08-14 15:38:04 +0300

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((first, second) => {
+        const firstAge = getAge(first.yearOfBirth, first.yearOfDeath);
+        const secondAge = getAge(second.yearOfBirth, second.yearOfDeath);
+        return firstAge > secondAge ? first : second;
+    });
 
+    function getAge(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        };
+        return death - birth;
+    };
 };
 
 // Do not edit below this line
